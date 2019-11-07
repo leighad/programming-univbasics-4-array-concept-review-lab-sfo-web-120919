d55db42bda296da1d105f7d2acbaf2f66e09ce19
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-07 23:53:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -32,11 +32,11 @@ def find_max_value(array)
   sorted_array.last
 end
 
-def find_min_value(array)
-  # Add your solution here
-  min =  
-  array.length.times do |index|
-    if array[index] < 
+# def find_min_value(array)
+#   # Add your solution here
+#   min =  
+#   array.length.times do |index|
+#     if array[index] < 
   
   
-end
+# end
