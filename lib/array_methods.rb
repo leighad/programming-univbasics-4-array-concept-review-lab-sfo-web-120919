d1850d3deb2ae9d203b32a1e5d72e4bc3473736f
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-07 23:56:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -38,6 +38,9 @@ def find_min_value(array)
   array.length.times do |index|
     if array[index] == 0 
       min = array[index]
+    else
+      if array[index] < min 
+        min = array[index]
     end
   end
  min 
